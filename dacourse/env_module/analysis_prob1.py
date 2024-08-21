b2e832a4d578e9cbad7227197051166efcc22613
n <>
2024-08-20 22:18:09 -0400

    update

--- dacourse/env_module/analysis_prob1.py
+++ dacourse/env_module/analysis_prob1.py
@@ -116,35 +116,93 @@ print(f' min var x coord {min_idx[1]*3} km , y coord {min_idx[0]*3} km')
 # create image plot
 def implot(data2d):
     fig, ax = plt.subplots()
-    pos = ax.imshow(data2d, origin='lower')
+    pos = ax.imshow(data2d, origin='lower', cmap='coolwarm')
     fig.colorbar(pos, ax=ax)
     plt.show()
 
 implot(d3_u[30,:,:])
+
+def savedebug(data, name):
+    plt.plot(data.reshape(-1))
+    plt.savefig('debug/' + name + '.png')
+
 
 # %%
 
 # Problem 2
 # calc correlation coeff
 
-corrmap = np.zeros((20,20))
-corrmap = corrmap.astype(float)
-for y in range(20):
-    for x in range(20):
-        d1 = d3_u[:,y,x]
-        cmap = np.zeros((20,20))
-        for y2 in range(20):
-            for x2 in range(20):
-                d2 = d3_u[:,y2,x2]
-                d12 = np.array([d1, d2])
-                corr = np.corrcoef(d12)[1,1]
-                if np.isnan(corr):
-                    print(x,y,x2,y2)
-                cmap[y2,x2] = corr
+# uses d3_u d3_v from above
+
+n_t = d3_u.shape[0]
+n_x = d3_u.shape[2]
+n_y = d3_u.shape[1]
+
+x_inds = list(range(0, n_x, 200))
+y_inds = list(range(0, n_y, 200))
+
+# for each x,y store x2,y2,corr of highest corr 
+# [x_coord x y_coord x [y2,x2,corrcoef]]
+hicorrmap = np.empty((n_y, n_x, 3)).astype(float)
+hicorrmap[:] = np.nan
+hicorrval = np.empty((n_y, n_x))
+hicorrval[:] = np.nan
+
+for y1 in y_inds:
+    for x1 in x_inds:
+        d1_u = d3_u[:,y1,x1]
+        d1_v = d3_v[:,y1,x1]
+        if all(d1_u==0) and all(d1_v==0):
+            continue
 
 
+        # corr vals for x1,y1 to all x2,y2's
+        cmap = np.empty((n_y, n_x)).astype(float)
+        cmap[:] = np.nan
+
+        print(f' find corr for xy {x1},{y1}')
+        # get corr for all x2,y2's
+        for y2 in y_inds:
+            for x2 in x_inds:
+                if y2 == y1 and x2 == x1:
+                    # this is corr of same
+                    continue
+                d2_u = d3_u[:,y2,x2]
+                d2_v = d3_v[:,y2,x2]
+
+                if all(d2_u==0) or all(d2_v==0):
+                    continue
+                
+                d12_u = np.array([d1_u, d2_u])
+                d12_v = np.array([d1_v, d2_v])
+                corr_u = np.corrcoef(d12_u)[0,1]
+                corr_v = np.corrcoef(d12_v)[0,1]
+                # print(f' corr u {corr_u}')
+                if np.isnan(corr_u) or np.isnan(corr_v):
+                    continue
+
+                cmap[y2,x2] = np.max([corr_u, corr_v])
+
+        # find highest corr, save it as hi corr for x1,y1
+        if np.all( np.isnan( cmap ) ):
+            continue    
+
+        savedebug(cmap, f'{x1}_{y1}')
+
+        hicorr = np.nanmax(cmap)
+        hicorrIdx = np.unravel_index( np.nanargmax(cmap), cmap.shape )
+        hicorrmap[y1, x1, :] = np.array([hicorrIdx[0], hicorrIdx[1], hicorr])
+        hicorrval[y1, x1] = hicorr
+        print(f' hi corr val y1 x1 val {y1} {x1} {hicorr}')
+    
+nonnan = hicorrval[~np.isnan(hicorrval)]
+plt.plot(nonnan)
 
 
 # for u-component 
+
+# %%
+# example array
+a = np.array(range(12)).reshape((3,4))
 
 # %%
